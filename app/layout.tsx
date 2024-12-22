e1d35bd0e9f850cef9edcba93167a7749a695e6c
Anass Bouassaba <ebouassaba@gmail.com>
2024-12-22 17:03:25 +0100

    wip: building layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import Link from 'next/link'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu'
-import { forwardRef } from 'react'
 import cx from 'clsx'
+import { NavigationBar } from '@/components/navigation-bar'
 import './globals.css'
 
 const geistSans = Geist({
@@ -29,44 +19,6 @@ export const metadata: Metadata = {
   description: 'A look into the future.',
 }
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Alert Dialog',
-    href: '/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
-  },
-  {
-    title: 'Hover Card',
-    href: '/docs/primitives/hover-card',
-    description:
-      'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: '/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: '/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: '/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: '/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-  },
-]
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -76,7 +28,6 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cx(
-          'h-full',
           'flex',
           'flex-col',
           geistSans.variable,
@@ -84,208 +35,64 @@ export default function RootLayout({
           'antialiased',
         )}
       >
-        <nav className={cx('p-4')}>
-          <div className={cx('container', 'mx-auto')}>
-            <NavigationMenu>
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul
-                      className={cx(
-                        'grid',
-                        'gap-3',
-                        'p-6',
-                        'md:w-[400px]',
-                        'lg:w-[500px]',
-                        'lg:grid-cols-[.75fr_1fr]',
-                      )}
-                    >
-                      <li className={cx('row-span-3')}>
-                        <NavigationMenuLink asChild>
-                          <Link
-                            className={cx(
-                              'flex',
-                              'h-full',
-                              'w-full',
-                              'select-none',
-                              'flex-col',
-                              'justify-end',
-                              'rounded-md',
-                              'bg-gradient-to-b',
-                              'from-muted/50',
-                              'to-muted',
-                              'p-6',
-                              'no-underline',
-                              'outline-none',
-                              'focus:shadow-md',
-                            )}
-                            href="/"
-                          >
-                            <div
-                              className={cx(
-                                'mb-2',
-                                'mt-4',
-                                'text-lg',
-                                'font-medium',
-                              )}
-                            >
-                              shadcn/ui
-                            </div>
-                            <p
-                              className={cx(
-                                'text-sm',
-                                'leading-tight',
-                                'text-muted-foreground',
-                              )}
-                            >
-                              Beautifully designed components that you can copy
-                              and paste into your apps. Accessible.
-                              Customizable. Open Source.
-                            </p>
-                          </Link>
-                        </NavigationMenuLink>
-                      </li>
-                      <ListItem href="/docs" title="Introduction">
-                        Re-usable components built using Radix UI and Tailwind
-                        CSS.
-                      </ListItem>
-                      <ListItem href="/docs/installation" title="Installation">
-                        How to install dependencies and structure your app.
-                      </ListItem>
-                      <ListItem
-                        href="/docs/primitives/typography"
-                        title="Typography"
-                      >
-                        Styles for headings, paragraphs, lists...etc
-                      </ListItem>
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul
-                      className={cx(
-                        'grid',
-                        'w-[400px]',
-                        'gap-3',
-                        'p-4',
-                        'md:w-[500px]',
-                        'md:grid-cols-2',
-                        'lg:w-[600px]',
-                      )}
-                    >
-                      {components.map((component) => (
-                        <ListItem
-                          key={component.title}
-                          title={component.title}
-                          href={component.href}
-                        >
-                          {component.description}
-                        </ListItem>
-                      ))}
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href="/docs" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Documentation
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
-        </nav>
-        <main
+        <div
           className={cx(
-            'flex-grow',
-            'container',
-            'mx-auto',
-            'px-4',
-            'py-6',
-            'overflow-y-auto',
-          )}
-        >
-          {children}
-        </main>
-        <footer
-          className={cx(
-            'h-32',
-            'p-4',
-            'bg-gray-900',
-            'text-white',
+            'fixed',
+            'top-0',
+            'left-0',
+            'w-full',
             'flex',
             'items-center',
-            'justify-center',
+            'h-16',
+            'backdrop-blur-sm',
+            'bg-white',
+            'bg-opacity-80',
+            'border-b',
+            'border-gray-200',
+            'z-50',
           )}
         >
+          <div className={cx('container', 'mx-auto')}>
+            <NavigationBar />
+          </div>
+        </div>
+        <main
+          className={cx(
+            'container',
+            'mx-auto',
+            'top-0',
+            'pt-24',
+            'overflow-y-auto',
+            'z-1',
+          )}
+        >
+          <article className={cx('w-3/5', 'mx-auto')}>{children}</article>
+        </main>
+        <footer className={cx('flex', 'items-center', 'justify-center')}>
           <div
             className={cx(
-              'h-full',
+              'container',
+              'h-24',
+              'shrink-0',
+              'py-7',
+              'mb-4',
+              'rounded-2xl',
+              'bg-gray-900',
+              'text-white',
               'flex',
               'flex-col',
-              'gap-2',
-              'container',
-              'mx-auto',
+              'items-center',
+              'justify-center',
             )}
           >
-            <div className={cx('grow')}></div>
-            <p>Copyright © 2025 Hosorcope.</p>
+            <div className={cx('w-3/5', 'mx-auto')}>
+              <p className={cx('text-sm', 'text-gray-400')}>
+                Copyright © 2025 Hosorcope.
+              </p>
+            </div>
           </div>
         </footer>
       </body>
     </html>
   )
 }
-
-const ListItem = forwardRef<
-  React.ElementRef<'a'>,
-  React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cx(
-            'block',
-            'select-none',
-            'space-y-1',
-            'rounded-md',
-            'p-3',
-            'leading-none',
-            'no-underline',
-            'outline-none',
-            'transition-colors',
-            'hover:bg-accent',
-            'hover:text-accent-foreground',
-            'focus:bg-accent',
-            'focus:text-accent-foreground',
-            className,
-          )}
-          {...props}
-        >
-          <div className={cx('text-sm', 'font-medium', 'leading-none')}>
-            {title}
-          </div>
-          <p
-            className={cx(
-              'line-clamp-2',
-              'text-sm',
-              'leading-snug',
-              'text-muted-foreground',
-            )}
-          >
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
-})
-ListItem.displayName = 'ListItem'
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import cx from 'clsx'
-
 export default function Home() {
   return (
-    <article className={cx('prose', 'max-w-3xl', 'mx-auto')}>
+    <>
       <h2>Scrollable Content</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -13,6 +11,91 @@ export default function Home() {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </p>
-    </article>
+      <p>
+        Morbi fermentum natoque ad maximus tristique sagittis vivamus congue
+        convallis. Ac urna est ac penatibus imperdiet praesent. Aliquet lorem
+        fusce placerat molestie lacus. Efficitur et torquent commodo ultrices
+        fringilla habitasse. Porttitor mus pharetra euismod litora diam
+        hendrerit sapien. Vulputate parturient natoque semper a pretium
+        sollicitudin leo. Mus lectus hac mauris mollis commodo purus litora erat
+        molestie. Conubia ex placerat netus cursus efficitur eu mollis.
+      </p>
+      <p>
+        Porttitor ante neque in curae; justo lacinia. Consequat duis sociosqu
+        dictum mi sagittis pharetra pretium mauris. Tempus dignissim ad
+        habitasse pulvinar habitant bibendum nisi ultrices. Lacinia malesuada
+        dolor scelerisque natoque pharetra praesent sapien tincidunt venenatis.
+        Aliquet nisl congue vivamus etiam mollis. Mollis amet bibendum nascetur
+        tincidunt conubia nunc. Mus nibh volutpat habitant potenti sit.
+      </p>
+      <p>
+        Parturient diam faucibus libero lacus per. Adipiscing dignissim congue
+        dolor cubilia mi amet. Quis nec gravida posuere elementum tempus maximus
+        neque vivamus nisl. Enim consectetur torquent; curae nibh lectus nostra
+        nostra. Imperdiet fermentum auctor posuere feugiat tempor ullamcorper
+        cubilia maximus feugiat. Class faucibus mauris ante facilisis facilisi.
+        Amet neque penatibus tristique iaculis dui volutpat litora. Netus enim
+        eleifend est euismod auctor et at consectetur. Orci sed ad tristique
+        ligula dignissim hac proin ridiculus tempus.
+      </p>
+      <p>
+        Himenaeos rutrum natoque dignissim metus tellus curae donec sapien
+        hendrerit. Lorem venenatis cubilia eros dictum tempus adipiscing lacinia
+        congue. Auctor ullamcorper neque rutrum ut arcu auctor est ipsum nec.
+        Urna erat magna condimentum porta habitasse. Litora enim maximus lectus
+        eu magna tristique ex vitae. Natoque interdum proin conubia etiam
+        lacinia porta. Tempor nostra nostra nam viverra purus odio a. Class
+        nulla aptent porttitor quam nisl. Dui ut facilisi sociosqu ipsum
+        sollicitudin eleifend.
+      </p>
+      <p>
+        Sed consequat erat ultricies quisque litora interdum. Justo mollis
+        mattis aliquet felis maecenas morbi maecenas dolor eget. Neque maximus
+        feugiat id pretium donec vulputate felis. Sollicitudin neque a massa
+        consequat ultricies duis rhoncus sociosqu enim. Faucibus tincidunt
+        fringilla ante egestas commodo tortor amet ut lacus. Arcu curae ipsum
+        quisque hac netus ullamcorper. Feugiat quam dictum ultricies placerat
+        aliquet praesent.
+      </p>
+      <p>
+        Lacus vestibulum erat cubilia ex proin leo pulvinar? Mattis erat
+        facilisis odio ipsum posuere iaculis primis inceptos. Varius risus orci
+        adipiscing adipiscing rhoncus tortor. Aenean mattis volutpat placerat,
+        id torquent quisque. Bibendum mi feugiat finibus ad venenatis? Aenean
+        ridiculus porttitor dignissim nunc, odio parturient nam diam. Molestie
+        class vel; sem maecenas vitae blandit senectus. Platea consectetur
+        auctor vitae magna gravida. Congue enim luctus suspendisse ultricies,
+        lobortis in. Ultricies sit rutrum lorem nec et magna.
+      </p>
+      <p>
+        Augue habitasse dui vivamus ultricies nullam quis mus himenaeos nibh.
+        Senectus per sem potenti ullamcorper nam sodales faucibus. Hac erat
+        gravida in dolor dolor diam, volutpat est dolor. Finibus felis commodo
+        montes; imperdiet dictumst ipsum sit volutpat. Et nascetur per sit proin
+        inceptos, ligula ante. Ridiculus id montes phasellus faucibus pharetra
+        elementum egestas lacinia. Dui felis vestibulum nunc bibendum viverra
+        turpis curabitur enim. Ullamcorper eget lorem libero eros rutrum ex.
+        Morbi placerat turpis faucibus pellentesque litora hac.
+      </p>
+      <p>
+        Rhoncus dignissim risus, ligula cubilia curabitur ante. Donec cubilia
+        lacinia nisi fringilla quisque auctor; bibendum augue. Pulvinar potenti
+        efficitur nascetur leo condimentum maximus litora dui. Condimentum
+        bibendum himenaeos potenti urna fermentum vivamus vivamus. Fames
+        adipiscing suspendisse lorem integer augue hac. Aptent per efficitur
+        duis amet taciti condimentum. Condimentum mattis mi ullamcorper
+        adipiscing lectus auctor. Habitasse praesent phasellus vestibulum;
+        conubia torquent platea per? Massa in amet nisi tellus duis est.
+      </p>
+      <p>
+        Sapien habitant cubilia cursus malesuada ipsum consectetur consectetur.
+        Sit condimentum ad taciti sit tempus dignissim. Dis porttitor integer
+        primis lacinia sem placerat elementum blandit. Proin posuere ultrices
+        rhoncus elementum eget mi mollis interdum. Pulvinar aenean turpis
+        senectus malesuada, nisi cursus justo et. Nascetur felis sollicitudin
+        leo aliquam donec lorem dapibus. Vehicula etiam eleifend nisi eget ante
+        venenatis, tristique elit.
+      </p>
+    </>
   )
 }
